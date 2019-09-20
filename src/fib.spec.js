606b2bf56a7e39cc0e8bc7036e243c16b80386c1
Cesousan <cesar.sabater@tiny-atom.com>
2019-09-20 11:01:34 +0200

    working

--- src/fib.spec.js
+++ src/fib.spec.js
@@ -1,6 +1,11 @@
 const fib = require('./fib').fib;
 
 describe('fibonnaci', () => {
+  
+  it('should return 1 if passed 0', () => {
+    expect(fib(0)).toEqual(1);    
+  })
+
   it('should return 1 if passed 1', () => {
     expect(fib(1)).toEqual(1)
   });

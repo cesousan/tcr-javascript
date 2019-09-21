d7daf5b5fd9dfbf7598428f2b436285d03dafa99
Cesousan <cesar.sabater@tiny-atom.com>
2019-09-22 00:03:10 +0200

    working

--- src/main/sort.js
+++ src/main/sort.js
@@ -10,6 +10,15 @@ function sort(el, sortOrder) {
   }
 }
 
+function sortString(str, order) {
+  const strArr = str.split('').sort();
+  return order === false ? strArr.reverse().join() : strArr.join();
+}
+
+function sortArray(arr, order) {
+  return arr.sort((a, b) => order === false ? b - a : a - b);
+}
+
 module.exports = {
   sort 
 }; 
